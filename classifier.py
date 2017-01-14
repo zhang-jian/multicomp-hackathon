a8fd4223dd4c7eaf4029f3d419134a518ee9f699
Alexandria Katarina Vail <avail@atlas4.multicomp.cs.cmu.edu>
2017-01-13 20:28:09 -0500

    fixing jayanth's mistakes

--- classifier.py
+++ classifier.py
@@ -1,14 +1,25 @@
+import math
 import pickle
 
 from keras.models import Sequential
 from keras.layers import Convolution2D, MaxPooling2D, Flatten, Dense
+from keras.utils.np_utils import to_categorical
 
+from sklearn.externals import joblib
+
+from utils import *
+
+NUM_SECONDS = 3
 NUM_CHANNELS = 1025
-NUM_TIMEPOINTS = 431
 NUM_GENRES = 10
+
+NUM_TIMEPOINTS = math.ceil(43.1 * NUM_SECONDS)
 
 MODEL_FN = 'model.h5'
 HISTORY_FN = 'history.pkl'
+
+# x, y = load_dataset('/multicomp/datasets/GTZAN', NUM_SECONDS, 5)
+x, y = joblib.load('xy_3s_5.pkl')
 
 model = Sequential()
 
@@ -29,11 +40,12 @@ model.compile(optimizer='adam', loss='categorical_crossentropy',
 
 # model.summary()
 
-history = model.fit(x=x, y=y, batch_size=32, nb_epoch=10,
+history = model.fit(x=x, y=to_categorical(y), batch_size=32, nb_epoch=10,
                     verbose=1, validation_split=0.2, shuffle=True)
 pred = model.predict_classes(x=x, batch_size=32, verbose=1)
 acc = np.mean(y == pred)
+print("Accuracy: {}".format(acc))
 
 model.save_weights(MODEL_FN)
-with open(HISTORY_FN, 'w') as outfile:
-    pickle.dump(history, outfile)
+with open(HISTORY_FN, 'wb') as outfile:
+    pickle.dump(history.history, outfile)
